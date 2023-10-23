ee0c00c6f7a9e82bf59b7822fb604b192a64e724
Ferolek03 <melkonyaynsam@gmail.com>
2023-10-23 23:57:54 +0700

    Initial commit

---
+++ router/router.js
@@ -0,0 +1,26 @@
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+
+// import DarkTema from '../src/components/DarkTema.vue'
+// import TheWelcome from '../src/components/TheWelcome.vue'
+
+
+// Vue.use(VueRouter);
+
+// const routes = [
+//   { 
+//     path: '/index.html',
+//     name: 'index',
+//     component: DarkTema 
+//     },
+//   { 
+//     path: '/login',
+//     name: 'login',
+//     component: TheWelcome },
+// ];
+
+// const router = new VueRouter({
+//   routes,
+// });
+
+// export default router;
